23cd29cff8860d5afc5c027f918efe5bcea820fc
Nicolas Jiang <nicolas.jiang@9spokes.com>
2019-04-23 13:20:39 +1200

    add favicon

--- packages/next/pages/_document.tsx
+++ packages/next/pages/_document.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import Document, {
-  NextDocumentContext,
-  Html,
-  Head,
-  Main,
-  NextScript,
-} from 'next/document';
+import Document, { NextDocumentContext, Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -41,11 +35,8 @@ export default class MyDocument extends Document {
             name="keywords"
             content="音乐,QQ音乐,在线听歌,音乐下载,音乐播放器,音乐网站,MV,巅峰榜,音乐排行榜,翻译歌曲,热门歌曲,经典老歌,无损音乐,无损曲库. 网易云音乐，音乐，播放器，网易，下载，播放，DJ，免费，明星，精选，歌单，识别音乐，收藏，分享音乐，音乐互动，高音质，320K，音乐社交，官网，music.163.com"
           />
-
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=UA-138145876-1"
-          />
+          <link rel="icon" type="image/x-icon" href="../static/favicon.ico" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138145876-1" />
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -62,11 +53,7 @@ export default class MyDocument extends Document {
             }}
           />
           <link rel="stylesheet" type="text/css" href="/static/css/font.css" />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/css/nprogress.css"
-          />
+          <link rel="stylesheet" type="text/css" href="/static/css/nprogress.css" />
         </Head>
         <body>
           <Main />
